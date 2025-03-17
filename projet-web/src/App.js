4b46154b6f3087d12f7dca78eda4a72cd7ee168c
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-03-17 22:02:20 +0100

    début d'implémentation du plan, seulement HTML pour l'instant

--- projet-web/src/App.js
+++ projet-web/src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
+import image_logo_insa from './logo-ng.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>INSApprovisionnement</h1>
+      </div>
+      <div class = "Partie-fonctionnelle">
+        <div class="Ajoute-produit">
+          <h2>Ajouter Produit</h2>
+          <input class="Inventory-input" type="text" value="Nom du produit"></input>
+          <input class="Inventory-input" type="number" value="1"></input>
+          <input class="Inventory-input" type="date" value="Date de péremption"></input>
+          <button class="Inventory-input-button">Entrer</button>
+        </div>
+        <div class="Tableau-et-boutons">
+          <div class="Tableau">
+           <h2>L'inventaire</h2>
+           <div class="Inventory-row">
+            <h3 class="Inventory-cell">Produit</h3>
+            <h3 class="Inventory-cell">Quantité</h3>
+            <h3 class="Inventory-cell">Date de péremption</h3>
+           </div>
+          </div>
+          <div class = "Boutons-en-bas">
+            <button class="Bouton-tableau">
+              Sauvegarder
+            </button>
+            <button class="Bouton-tableau">
+              Version la plus récente
+            </button>
+          </div>
+        </div>
+      </div>
+      <header class="App-header">
+        <img src={logo} class="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +49,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer class="BasDePage">
+        <img src={image_logo_insa} alt="logo INSA" className="INSA-logo"/>
+      </footer>
     </div>
+    
   );
 }
 
