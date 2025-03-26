16d62c36ba6e30627af71e2085aed0207ada08df
MidoliC <midolicaillon@gmail.com>
2025-03-26 15:12:53 +0100

    start complet sur windows et début de end

--- end.ps1
+++ end.ps1
@@ -1,4 +1,6 @@
 docker kill mongo-test
 $myLabel = 'MyImportantScript'
 $p       = Get-Process -Name 'powershell' | Where-Object CommandLine -like "*-CustomPipeName '$myLabel'*"
-Stop-Process -ID ($p).id -Force
+
+#echo $p
+#Stop-Process -ID ($p).id -Force
